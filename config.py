fd8cf3b4fadb1ee5da8e6485d64db1d65d3912ea
Rithika <rithika7092005@gmail.com>
2026-06-23 15:37:31 +0530

    Fix Mongo URI

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 class Config:
     SECRET_KEY = "amazon_secret_key"
 
-    MONGO_URI = MONGO_URI = "mongodb+srv://rithika7092005_db_user:rithi2005@cluster0.cxpi92s.mongodb.net/AmazonDB?retryWrites=true&w=majority&appName=Cluster0"
+    MONGO_URI = "mongodb+srv://rithika7092005_db_user:rithi2005@cluster0.cxpi92s.mongodb.net/AmazonDB?retryWrites=true&w=majority"
 
     UPLOAD_FOLDER = "static/uploads"
